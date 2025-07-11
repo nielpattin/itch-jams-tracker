9fccf49f9130a88e3d78fb1ff3b9c43b3f4a75fa
Niel Tran <66520542+nielpattin@users.noreply.github.com>
2025-07-11 16:22:09 +0700

    feat: add @vercel/analytics integration and update package manager version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,12 @@
 	import '../app.css';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { ModeWatcher } from 'mode-watcher';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	let { children } = $props();
+	console.log('MODE:', dev ? 'development' : 'production');
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <ModeWatcher defaultMode={'dark'} />
